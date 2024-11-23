d1777d687eb55cdfb32983470b8f60f48dc04337
SAW-COIN <abasfacre@gmail.com>
2024-11-24 01:55:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// منع القائمة السياقية
+        document.addEventListener("contextmenu", function(event) {
+            event.preventDefault();
+        });
+
+        // منع الضغط المطوّل على العناصر
+        document.querySelectorAll("img, p").forEach(element => {
+            element.addEventListener("mousedown", e => e.preventDefault());
+            element.addEventListener("touchstart", e => e.preventDefault());
+        });
+
+
+
+
 const scoreDisplay = document.getElementById('score');
 const missedCountDisplay = document.getElementById('missedCount');
 const timerDisplay = document.getElementById('timer');
